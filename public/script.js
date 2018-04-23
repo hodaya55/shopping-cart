3b3539bc3e116f953793515803ad6ac83b5cc00a
hodaya55 <hodayah159@gmail.com>
2018-04-23 18:18:39 +0300

    update

--- public/script.js
+++ public/script.js
@@ -2,17 +2,17 @@
 var cart = [];
 
 var updateCart = function () {
-  // TODO: Write this function. In this function we render the page.
+  // Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
 
   $('.cart-list').empty();
-  $('.total').text(0);
+  // $('.total').text(0);
 
   var total = 0;
   for (var i in cart) {
-    $('.cart-list').append('<span>' + cart[i].name + ' - ' + cart[i].price + '</span> <br>');
-    total += parseInt(cart[i].price.slice(2));
+    $('.cart-list').append('<span>' + cart[i].name + ' - $' + cart[i].price + '</span> <br>');
+    total += cart[i].price;
   }
 
   $('.total').text(total);
@@ -21,7 +21,7 @@ var updateCart = function () {
 
 
 var addItem = function (item) {
-  // TODO: Write this function. Remember this function has nothing to do with display. 
+  // Write this function. Remember this function has nothing to do with display. 
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
 
   cart.push(item);
@@ -29,30 +29,21 @@ var addItem = function (item) {
 }
 
 var clearCart = function () {
-  // TODO: Write a function that clears the cart ;-)
+  // Write a function that clears the cart ;-)
   $('.total').text(0); // set back the total to $ 0
   cart.length = 0; // empty all the cart array 
   $('.cart-list').empty(); // empty the cartlist from page
 }
 
-var toggle = false;
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
-  // $('.shopping-cart').toggleClass('.show');
+  // hide/show the shopping cart!
 
-  if (!toggle) {
-    toggle = true;
-    $('.shopping-cart').css('display', 'block');
-  }
-  else {
-    toggle = false;
-    $('.shopping-cart').css('display', 'none');
-  }
+  $('.shopping-cart').toggleClass('show');
 });
 
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
+  //  get the "item" object from the page
   var $clickedItem = $(this).closest('.card.item');
 
   console.log($clickedItem.find('.price').text());
@@ -60,7 +51,7 @@ $('.add-to-cart').on('click', function () {
 
   var item = {
     name: $clickedItem.data().name,
-    price: $clickedItem.find('.price').text()
+    price: $clickedItem.data().price
   }
   addItem(item);
   updateCart();
@@ -73,9 +64,9 @@ $('.clear-cart').on('click', function () {
 // update the cart as soon as the page loads!
 updateCart();
 
-
+// when the web page is max-width 740px
 $('.navbar-toggle.collapsed').on('click', function () {
-  $('.shopping-cart').css('display', 'block');
   $('.view-cart').css('display', 'none');
-  // $('.shopping-cart').toggleClass('.show');
+  $('.nav.navbar-nav.navbar-right').css('display', 'none');
+  $('.shopping-cart').toggleClass('show');
 })
