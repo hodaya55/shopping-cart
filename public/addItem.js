d95bcade1ad2811a6cedd4b21744f02d3afbd289
hodaya55 <hodayah159@gmail.com>
2018-04-25 15:42:52 +0300

    update

--- public/addItem.js
+++ public/addItem.js
@@ -1,24 +1,38 @@
 
 
-var addItem = function () {
+var addProduct = function () {
+    console.log('Im in add product');
+
     var name = $('#name').val();
     var src = $('#src').val();
     var price = $('#price').val();
 
-console.log(name);
+//     var productArrayName= [];
+//   var n= $('.container').find('.card.item').data().name;
+//   productArrayName.push(n);
+//     if (name in productArrayName) {
+//         $('#name').css('color', 'red');
+//         $('#error').toggleClass('show');
+//     }
 
-    var s1 = '<div class="col-md-4"> <div class="card-container"> ';
+//     else
+    
+    {
+        var s1 = '<div class="col-md-4"> ' +
+            '<div class="card-container"> ';
+        var s2 = '<div class="card item" item data-name=" ' + name + '" data-price="' + price + '"> ';
+        var s3 = '<div class="pricebox"> ' +
+            '<p class="price"> $' + price + '</p>';
+        var s4 = '  </div><div class="buybox"> <p class="add-to-cart"> ADD TO CART </p> </div>';
+        var s5 = '<div class="card-inner">' +
+            ' <img src="' + src + '" class="proimage">';
+        var s6 = '  <img  class="proimage"> </div>  </div> </div> </div> ';
 
-    var s2 = '<div class="card item" item data-name=" ' + name + '" data-price="' + price + '"> ';
-    var s3 = '<div class="pricebox"> <p class="price"> $' + price + '</p>';
-    var s4 = '  </div><div class="buybox"> <p class="add-to-cart"> ADD TO CART </p> </div>';
-    var s5 = '<div class="card-inner">' +
-        ' <img src="' + src + '" class="proimage">';
-    var s6 = '  <img  class="proimage"> </div>  </div> </div> </div> ';
-
-    $('.container').append(s1+s2+s3+s4+s5+s6);
-
+        $('.container').append(s1 + s2 + s3 + s4 + s5 + s6);
+    }
 
 }
 
-$('#AddBtn').click(addItem);
+$('#AddBtn').click(addProduct);
+
+
